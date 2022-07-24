c4f35d0b1e2d7a72bdf26f79ff1a338907917ce9
allanShady <acamilo.ag@gmail.com>
2022-07-24 23:15:17 +0200

    refactor: handler

--- src/server.go
+++ src/server.go
@@ -9,10 +9,12 @@ import (
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(context echo.Context) error {
-		app := context.Request().Header.Get("app")
-		return context.String(http.StatusOK, app)
-	})
+	e.GET("/", handler)
 
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func handler(context echo.Context) error {
+	app := context.Request().Header.Get("app")
+	return context.String(http.StatusOK, app)
 }
